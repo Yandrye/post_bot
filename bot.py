7e483aa506db2ae0f4fd42a5788230bf6e0579f9
RathHunt <sanchezvelazquezjoseangel@gmail.com>
2021-08-06 23:44:33 -0400

    fix fuction name

--- bot.py
+++ bot.py
@@ -318,19 +318,19 @@ def editar(message: Message, t: str, temp: Temp):
         db.set_temp(message.chat.id,temp)
         post_e(temp,message.chat.id,temp.markup if temp.markup else markup_e())
 
+def make_message_body(temp: Temp):
+    tt = []
 
-def post_e(temp,id,markup=None):
-    tt=[]
-    def aj(txt,var):
-        if var:tt.append( txt.format(var))
+    def aj(txt, var):
+        if var: tt.append(txt.format(var))
 
-    tit=':radioactive:{0} {1}\n\n'.format(
+    tit = ':radioactive:{0} {1}\n\n'.format(
         '({0})'.format(temp.post.tipo[0]) if temp.post.tipo else '',
         '<b>{0}</b>'.format(temp.post.titulo) if temp.post.titulo else ':expressionless:')
 
     tt.append(tit)
-    aj(':heavy_check_mark:Tipo: <b>{0}</b>\n',temp.post.tipo)
-    aj(':heavy_check_mark:Formato: <b>{0}</b>\n',temp.post.format)
+    aj(':heavy_check_mark:Tipo: <b>{0}</b>\n', temp.post.tipo)
+    aj(':heavy_check_mark:Formato: <b>{0}</b>\n', temp.post.format)
     aj(':heavy_check_mark:Episodios: <b>{0}</b>\n', temp.post.episodes)
     aj(':heavy_check_mark:Temporada: <b>{0}</b>\n', temp.post.temporada)
     aj(':heavy_check_mark:Tomo: <b>{0}</b>\n', temp.post.tomos)
